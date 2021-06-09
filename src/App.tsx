f06cace70b9d82233ff932231110df2b6dc7bba1
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-06-09 20:40:27 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, MemoryRouter, Route, Switch } from 'react-router-dom'
-import Login from './pages/Login'
-import Main from './pages/Main'
+import {
+  BrowserRouter as Router,
+  MemoryRouter,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Login from './pages/Login';
+import Main from './pages/Main';
 import CreateUser from './pages/CreateUser';
 // import CreateUser from './pages/CreateUser'
 
-
-function App() {
+const App: React.FC<{}> = () => {
   return (
     <div className="App">
       <MemoryRouter>
@@ -15,14 +19,14 @@ function App() {
           {/* <Login /> */}
           <Switch>
             {/* <Route exact path='/Main/:name' component={Main} /> */}
-            <Route exact path='/' component={Login} />
-            <Route exact path='/createUser/:new!' component={CreateUser} />
-            <Route exact path='/Main' component={Main} />
+            <Route exact path="/" component={Login} />
+            <Route exact path="/createUser/:new!" component={CreateUser} />
+            <Route exact path="/Main" component={Main} />
             {/* <Route exact path='/CreateUser/' component={CreateUser} /> */}
           </Switch>
         </Router>
       </MemoryRouter>
-    </div >
+    </div>
   );
-}
+};
 export default App;
