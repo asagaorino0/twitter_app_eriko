9569af26ea3df00abe22d043e22a6544093d7fc7
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-07-04 21:02:48 +0900

    [add]LINE_oshirase

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -231,35 +231,42 @@ const Header = () => {
     const classes = useStyles();
 
     const sendMessage = function (e) {
-        if (liff.isApiAvailable('shareTargetPicker')) {
-            liff.shareTargetPicker([
-                {
-                    'type': 'text',
-                    'text': 'Hello, World!'
-                }
-            ])
-                .then(function (res) {
-                    if (res) {
-                        // succeeded in sending a message through TargetPicker
-                        console.log(`[${res.status}] Message sent!`)
-                    } else {
-                        const [majorVer, minorVer] = (liff.getLineVersion() || "").split('.');
-                        if (parseInt(majorVer) == 10 && parseInt(minorVer) < 11) {
-                            // LINE 10.3.0 - 10.10.0
-                            // Old LINE will access here regardless of user's action
-                            console.log('TargetPicker was opened at least. Whether succeeded to send message is unclear')
-                        } else {
-                            // LINE 10.11.0 -
-                            // sending message canceled
-                            console.log('TargetPicker was closed!')
-                        }
-                    }
-                }).catch(function (error) {
-                    // something went wrong before sending a message
-                    console.log('something wrong happen')
-                })
-        }
+        liff.shareTargetPicker([{
+            'type': 'text',
+            'text': 'Hello, World!'
+        }])
     }
+
+    // const sendMessage = function (e) {
+    //     if (liff.isApiAvailable('shareTargetPicker')) {
+    //         liff.shareTargetPicker([
+    //             {
+    //                 'type': 'text',
+    //                 'text': 'Hello, World!'
+    //             }
+    //         ])
+    //             .then(function (res) {
+    //                 if (res) {
+    //                     // succeeded in sending a message through TargetPicker
+    //                     console.log(`[${res.status}] Message sent!`)
+    //                 } else {
+    //                     const [majorVer, minorVer] = (liff.getLineVersion() || "").split('.');
+    //                     if (parseInt(majorVer) == 10 && parseInt(minorVer) < 11) {
+    //                         // LINE 10.3.0 - 10.10.0
+    //                         // Old LINE will access here regardless of user's action
+    //                         console.log('TargetPicker was opened at least. Whether succeeded to send message is unclear')
+    //                     } else {
+    //                         // LINE 10.11.0 -
+    //                         // sending message canceled
+    //                         console.log('TargetPicker was closed!')
+    //                     }
+    //                 }
+    //             }).catch(function (error) {
+    //                 // something went wrong before sending a message
+    //                 console.log('something wrong happen')
+    //             })
+    //     }
+    // }
 
     // const sendMessage = function (e) {
     //     // Check if shareTargetPicker is available
