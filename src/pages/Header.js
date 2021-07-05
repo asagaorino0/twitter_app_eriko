471e3bd9f5e50dc6e46a86a95b4e921364365cb7
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-07-05 09:10:50 +0900

    [add]LINE_通知

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -352,7 +352,7 @@ const Header = () => {
             </Button>
                 <button onClick={sendMessage} color="secondary">sendMessage</button>
                 <div class="line-it-button" data-lang="ja" data-type="share-b" data-ver="3" data-url="https://twitter-app-eriko.web.app" data-color="default" data-size="small" data-count="false" style="display: none;"></div>
-                <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script>
+                {/* <script src="https://www.line-website.com/social-plugins/js/thirdparty/loader.min.js" async="async" defer="defer"></script> */}
             </Toolbar>
             <Accordion>
                 <AccordionSummary
