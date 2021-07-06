706f21ece3f929abd0c80c08433a7e71307f7127
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-07-06 20:05:39 +0900

    [add]LINE_参加表明

--- src/pages/Follower.js
+++ src/pages/Follower.js
@@ -1,58 +1,12 @@
+
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Avatar from '@material-ui/core/Avatar';
 import firebase from "firebase/app"
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import liff from '@line/liff';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-        overflow: 'hidden',
-        padding: 'spacing(0, 3)',
-        backgroundColor: '#fff',
-    },
-    paper: {
-        maxWidth: 400,
-        margin: '5px 0px 5px 0px ',
-        padding: '16px',
-        boxShadow: '0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)',
-    },
-    pos: {
-        marginBottom: 0,
-    },
-    pink: {
-        color: '#fff',
-        backgroundColor: 'pink',
-    },
-    followPink: {
-        color: '#fff',
-        backgroundColor: 'pink',
-        fontSize: '10px',
-        borderRadius: '50%',
-        width: '20px',
-        height: '20px'
-    },
-    yellow: {
-        color: 'yelloW',
-        // backgroundColor: 'yelloW',
-    },
-    largePink: {
-        width: '40px',
-        height: '40px',
-        color: '#fff',
-        backgroundColor: 'pink',
-    },
-}));
-
 export default function SimplePaper({ followers }) {
-    const myLiffId = "1656149559-xXM4l4Gp"
-    const classes = useStyles();
-    const db = firebase.firestore();
-    const doc = firebase.firestore();
-    const messages = firebase.firestore();
     const [user, setUser] = useState([]);
     const [name, setName] = useState('');
     const [nName, setNName] = useState('');
@@ -74,7 +28,7 @@ export default function SimplePaper({ followers }) {
                                 doc.data()
                         });
                         setUser(user)
-                        // console.log(user)
+                        console.log("forowers_user", user)
                     })
             })
 
@@ -85,7 +39,7 @@ export default function SimplePaper({ followers }) {
         setAnchorFl(event.currentTarget);
         console.log(followers.length)
         console.log(followers.follower)
-        console.log(followers.nName)
+        console.log(followers.name)
     };
     const handleClose = () => {
         // setAnchorEl(null);
@@ -95,12 +49,7 @@ export default function SimplePaper({ followers }) {
     return (
         <div>
             <Grid container direction="row" justify="flex-start" alignItems="flex-end" >
-                {`${followers.follower}`.length !== 1 &&
-                    <img src={followers.follower} alt="" style={{ borderRadius: '50%', width: '20px', height: '20px' }} onClick={handleFollower} />
-                }
-                {`${followers.follower}`.length === 1 &&
-                    <Avatar className={classes.followPink} onClick={handleFollower}  >{followers.follower} </Avatar>
-                }
+                <img src={followers.follower} alt="" style={{ borderRadius: '50%', width: '20px', height: '20px' }} onClick={handleFollower} />
             </Grid>
             <Menu
                 id="simple-menu"
@@ -109,16 +58,9 @@ export default function SimplePaper({ followers }) {
                 open={Boolean(anchorFl)}
                 onClose={handleClose}
             >
-                {`${followers.follower}`.length === 1 &&
-                    <MenuItem onClick={handleClose} >
-                        <Avatar className={classes.largePink}>{followers.follower}</Avatar>
-                    </MenuItem>
-                }
-                {`${followers.follower}`.length !== 1 &&
-                    <MenuItem onClick={handleClose}>
-                        <img src={`${followers.follower}`} alt="" style={{ borderRadius: '50%', width: '80px', height: '80px' }} />
-                    </MenuItem>
-                }
+                <MenuItem onClick={handleClose}>
+                    <img src={`${followers.follower}`} alt="" style={{ borderRadius: '50%', width: '80px', height: '80px' }} />
+                </MenuItem>
                 <MenuItem onClick={handleClose}>{`${followers.followerName}`}</MenuItem>
             </Menu>
         </div>
